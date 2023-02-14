be899c4cc22924384ad64a0ea4856fd7dad60944
MINJI <97995420+whiterisi@users.noreply.github.com>
2023-02-14 20:39:33 +0900

    Create programmers주차요금계산_김민지.java

---
+++ 23.02.14/programmers주차요금계산_김민지.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+class Solution {
+    static int basicMin, basicFee, unitMin, unitFee;
+    public int[] solution(int[] fees, String[] records) {
+        basicMin = fees[0];
+        basicFee = fees[1];
+        unitMin = fees[2];
+        unitFee = fees[3];
+        
+        HashMap<Integer,int []> map = new HashMap<>();
+        HashMap<Integer, Integer> result  = new HashMap<>();
+        Set<Integer> carNum = new TreeSet<>();
+        
+        for(int i=0;i<records.length; i++){
+            StringTokenizer st = new StringTokenizer(records[i]);
+            
+            int [] time = new int [2];
+            String record = st.nextToken();
+            time[0]=Integer.parseInt(record.substring(0,2));
+            time[1]=Integer.parseInt(record.substring(3,5));
+            
+            int car = Integer.parseInt(st.nextToken());
+            String check = st.nextToken();
+            if(check.equals("out")){
+                result.put(car, calculateFee(map.get(car), time));
+                map.remove(car);
+            }else{
+                map.put(car, time);
+                carNum.add(car);
+            }
+        }
+        for(Map.Entry<Integer, int []> tmp : map.entrySet()){
+            int [] out = {23, 59};
+            result.put(tmp.getKey(), calculateFee(tmp.getValue(), out));
+        }
+      
+        int [] answer = new int [carNum.size()];
+        int idx=0;
+        for(int num: carNum){
+            answer[idx++]=num;
+        }
+        return answer;
+    }
+    public static int calculateFee(int [] in, int [] out){     
+        //분계산
+        int total =out[1]-in[1];
+        int fee=basicFee;
+        if(total<0){
+             out[0]-=1;
+            total+=60;
+        }
+        //시간계산
+        int tmp = out[0]-in[0];
+        total+=60*tmp;
+        //요금게산
+        if(total<=basicMin){
+            return fee;
+        }
+        total-=basicMin;
+        fee = (total-unitMin)*unitFee;
+        return fee;
+    }
+}
